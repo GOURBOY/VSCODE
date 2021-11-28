d5236c5c4922819a832a21bae74c219792f005e2
GOURBOY <abhaygour20253@acropolis.in>
2021-11-29 01:58:16 +0530

    commited

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,29 +1,14 @@
-#include<stdio.h>
-struct node{
-	int info;
-	struct node* link;
-}
-struct node* start=NULL;
-void traverse()
-{  struct node* temp;
-     if(start==NULL){
-		 printf("\nList is Empty.\n");
-	 }
-	 else{
-		 temp=start;
-		 while(temp!=NULL){
-			 printf("\nData is: %d\n",temp->info);
-		     temp=temp->link;
-		 }
-	 }
-}
-void insertAtPosition(){
-	struct node* temp,newnode;
-	int pos,data,i=1;
-	newnode=malloc(sizeof(struct node));
-    temp=start;
-	printf("\nEnter position and Data: ");
-	scanf("%d %d",&pos,&data);
-	
+#include<iostream>
+using namespace std;
+int main(){
+try{
+	float f1 = new float("3.0");
+	int x = f1.intValue();
+	byte b = f1.byteValue();
+	double d = f1.doubleValue();
+cout<<x+b+d;
+	}catch (NumberFormatException e) /* Line 9 */{
+		cout<<"bad number"; /* Line 11 */
+		}
 
 }
