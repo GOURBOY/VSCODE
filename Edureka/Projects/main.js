19d91b4925ffa5a090dfb914fee9d201d5986402
GOURBOY <abhaygour20253@acropolis.in>
2021-12-01 02:14:09 +0530

    Commit

---
+++ Edureka/Projects/main.js
@@ -0,0 +1,131 @@
+var but1=document.getElementById('but1');
+var but1=document.getElementById('but1');
+var but1=document.getElementById('but1');
+var but1=document.getElementById('but1');
+var but1=document.getElementById('but1');
+var but1=document.getElementById('but1');
+var but7=document.getElementById('but7');
+function fun1(){
+  but1.style.backgroundColor="#8c96ab"; 
+  but1.style.color="white";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+  
+}
+function fun2(){
+  but2.style.backgroundColor="#8c96ab";  
+  but2.style.color="white";
+
+  but1.style.backgroundColor="white"; 
+  but1.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+}
+function fun3(){
+  but3.style.backgroundColor="#8c96ab";  
+  but3.style.color="white";
+
+  but1.style.backgroundColor="white"; 
+  but1.style.color="#8b92a3";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+}
+function fun4(){
+  but4.style.backgroundColor="#8c96ab";  
+  but4.style.color="white";
+
+  but1.style.backgroundColor="white"; 
+  but1.style.color="#8b92a3";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+}
+function fun5(){
+  but5.style.backgroundColor="#8c96ab";  
+  but5.style.color="white";
+
+  but1.style.backgroundColor="white"; 
+  but1.style.color="8b92a3";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+}
+function fun6(){
+  but6.style.backgroundColor="#8c96ab";  
+  but6.style.color="white";
+
+  but1.style.backgroundColor="#white"; 
+  but1.style.color="8b92a3";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+ 
+  but7.style.backgroundColor="white"; 
+  but7.style.color="#8b92a3";
+}
+function fun7(){
+  but7.style.backgroundColor="#8c96ab";  
+  but7.style.color="white";
+
+  but1.style.backgroundColor="#white"; 
+  but1.style.color="8b92a3";
+  but2.style.backgroundColor="white"; 
+  but2.style.color="#8b92a3";
+  but3.style.backgroundColor="white"; 
+  but3.style.color="#8b92a3";
+  but4.style.backgroundColor="white"; 
+  but4.style.color="#8b92a3";
+  but5.style.backgroundColor="white"; 
+  but5.style.color="#8b92a3";
+  but6.style.backgroundColor="white"; 
+  but6.style.color="#8b92a3";
+ 
+}
