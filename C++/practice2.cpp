16a9393fa07539b31895b2824b09a9977f70e044
GOURBOY <abhaygour20253@acropolis.in>
2021-12-04 09:27:07 +0530

    Commited

--- C++/practice2.cpp
+++ C++/practice2.cpp
@@ -1,22 +1,76 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
- 
-int main()
-{
-    int N; cin>>N;
-    int p1, p1_total = 0, p2, p2_total = 0, winner, max_lead = 0;
-    for(int i=0; i<N; i++)
-    {
-        cin>>p1>>p2;
-        p1_total += p1; p2_total += p2; 
-        int temp = p2_total-p1_total; 
-        if (max_lead<abs(temp))
-        {
-            max_lead = abs(temp);
-            winner = temp>0 ? 2 : 1;
-        }
+// Adjascency List representation in C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int vertex;
+  struct node* next;
+};
+struct node* createNode(int);
+
+struct Graph {
+  int numVertices;
+  struct node** adjLists;
+};
+
+// Create a node
+struct node* createNode(int v) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->vertex = v;
+  newNode->next = NULL;
+  return newNode;
+}
+
+// Create a graph
+struct Graph* createAGraph(int vertices) {
+  struct Graph* graph = malloc(sizeof(struct Graph));
+  graph->numVertices = vertices;
+
+  graph->adjLists = malloc(vertices * sizeof(struct node*));
+
+  int i;
+  for (i = 0; i < vertices; i++)
+    graph->adjLists[i] = NULL;
+
+  return graph;
+}
+
+// Add edge
+void addEdge(struct Graph* graph, int s, int d) {
+  // Add edge from s to d
+  struct node* newNode = createNode(d);
+  newNode->next = graph->adjLists[s];
+  graph->adjLists[s] = newNode;
+
+  // Add edge from d to s
+  newNode = createNode(s);
+  newNode->next = graph->adjLists[d];
+  graph->adjLists[d] = newNode;
+}
+
+// Print the graph
+void printGraph(struct Graph* graph) {
+  int v;
+  for (v = 0; v < graph->numVertices; v++) {
+    struct node* temp = graph->adjLists[v];
+    printf("\n Vertex %d\n: ", v);
+    while (temp) {
+      printf("%d -> ", temp->vertex);
+      temp = temp->next;
     }
-    cout<<winner<<" "<<max_lead;
- 
+    printf("\n");
+  }
+}
+
+int main() {
+  struct Graph* graph = createAGraph(4);
+  addEdge(graph, 0, 1);
+  addEdge(graph, 0, 2);
+  addEdge(graph, 0, 3);
+  addEdge(graph, 1, 2);
+
+  printGraph(graph);
+
+  return 0;
 }
